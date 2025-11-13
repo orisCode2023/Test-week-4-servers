bdbae8e3188e88bddc74d14446b22a52bdb69230
odmark2023@gmail.com <odmark2023@gmail.com>
2025-11-13 09:58:38 +0200

    building the function structur and updating the requierment file

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+
+@app.get("/test")
+def greet():
+    return {"msg": "Hi from test"}
+
+
+@app.get("/tests")
+def get_test_name(name: str):
+    return {"msg": name}
+
+@app.post("/caesar")
+def get_decrypt_ot_encrypt():
+    pass
+
+@app.get()
+def encrypt(text: str):
+    pass
+
+@app.post()
+def decrypt():
+    pass
