1fecbfa909b94ce0ca5d18e915eac3859eca3c26
odmark2023@gmail.com <odmark2023@gmail.com>
2025-11-13 10:13:03 +0200

    adding a function to write to file and call it in the get name function

--- main.py
+++ main.py
@@ -2,24 +2,31 @@ from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
+def write_to_file(data):
+    with open("names.txt", 'a') as f:
+        f.write(f"\n {data}")
+
+def write_to_json():
+    pass
 
 @app.get("/test")
 def greet():
     return {"msg": "Hi from test"}
 
 
-@app.get("/tests")
+@app.get("/test/{name}")
 def get_test_name(name: str):
-    return {"msg": name}
+    write_to_file(name)
+    return {"msg": "saved user"}
 
-@app.post("/caesar")
-def get_decrypt_ot_encrypt():
-    pass
+# @app.post("/caesar")
+# def get_decrypt_ot_encrypt():
+#     pass
 
-@app.get()
-def encrypt(text: str):
-    pass
+# @app.get()
+# def encrypt(text: str):
+#     pass
 
-@app.post()
-def decrypt():
-    pass
+# @app.post()
+# def decrypt():
+#     pass
